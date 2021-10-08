e489891383a7ca6c2b348c4d05fc77f51034772e
solo7602@mail.ru <solo7602@mail.ru>
2021-10-08 13:08:57 +0400

    com

---
+++ labaBuldozerKazakovISEbd-22/labaBuldozerKazakovISEbd-22/enumWheel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace labaBuldozerKazakovISEbd_22
+{
+    public enum enumWheel
+    {
+        Four = 4,
+        Five = 5,
+        Six = 6
+    }
+}
