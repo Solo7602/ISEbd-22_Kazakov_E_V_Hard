5e22792961870d681bb1082a8ebbe9b481c6cf2f
solo7602@mail.ru <solo7602@mail.ru>
2021-11-02 12:38:45 +0400

    com

---
+++ labaBuldozerKazakovISEbd-22/labaBuldozerKazakovISEbd-22/FormBulldozer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace labaBuldozerKazakovISEbd_22
+{
+	public partial class FormBulldozer : Form
+	{
+		private IBulldozer bulldozer;
+		public FormBulldozer()
+		{
+			InitializeComponent();
+		}
+		private void Draw()
+		{
+			Bitmap bmp = new Bitmap(Picture.Width, Picture.Height);
+			Graphics gr = Graphics.FromImage(bmp);
+			bulldozer.DrawTransport(gr);
+			Picture.Image = bmp;
+		}
+		private void buttonCreate_Click(object sender, EventArgs e)
+		{
+			Random rnd = new Random();
+			bulldozer = new ModBuldozer(rnd.Next(100, 300), rnd.Next(1000, 2000), Color.Blue,
+		   Color.Yellow, true, true, comboBoxType.SelectedIndex, countWheels.SelectedIndex);
+			bulldozer.SetPosition(rnd.Next(100, 200), rnd.Next(100, 200), Picture.Width, Picture.Height);
+			Draw();
+		}
+		private void buttonMove_Click(object sender, EventArgs e)
+		{
+			//получаем имя кнопки
+			string name = (sender as Button).Name;
+			switch (name)
+			{
+				case "buttonUp":
+					bulldozer.MoveTransport(Direction.Up);
+					break;
+				case "buttonDown":
+					bulldozer.MoveTransport(Direction.Down);
+					break;
+				case "buttonLeft":
+					bulldozer.MoveTransport(Direction.Left);
+					break;
+				case "buttonRight":
+					bulldozer.MoveTransport(Direction.Right);
+					break;
+			}
+			Draw();
+		}
+    }
+}
---
+++ labaBuldozerKazakovISEbd-22/labaBuldozerKazakovISEbd-22/enum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace labaBuldozerKazakovISEbd_22
+{
+		public enum Direction
+		{
+			Up,
+			Down,
+			Left,
+			Right
+		}
+}
